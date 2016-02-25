21f06dbeebd8eda959e85b97aa857c4dd982c192
Juan Cardona <juancruz.23@hotmail.com>
2016-02-25 17:44:35 +0000

    agregada regex para obtener /new/ de url

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 var http = require('http');
-var fs = require("fs")
-
+var fs = require("fs");
+var path = require("path");
+var url = require("url");
 var server = http.createServer(function(req,res){
   if (req.url == "/"){
     fs.readFile("./client/index.html",function(err,data){
-      console.log("leyendo");
       if (err) {enviar404(res);}
       res.writeHead(200, {"content-type": "text/html; charset=UTF-8"});
       res.end(data);
-      console.log("ya lei");
     })
+  } else {
+    var match = req.url.match(/(^\/new\/)(.+)/);
+    if (match[1] == "/new/"){
+      console.log("siiiiiii!!!");
+    }
   }
 });
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
