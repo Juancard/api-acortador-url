300af7a25f119bb053c4daa87c1b4076e1b6715c
Juan Cardona <juancruz.23@hotmail.com>
2016-02-25 18:49:10 +0000

    se evalua path en caso que sea una url acortada

--- api/acortador.js
+++ api/acortador.js
@@ -2,12 +2,20 @@ var arregloUrl = {};
 var valorAcorte = 0;
 module.exports = function(req,res){
     var match = req.url.match(/(^\/new\/)(.+)/);
-    if (match[1] == "/new/"){
+    if (match && match[1] == "/new/"){
         var urlDada = match[2];
         var nuevaUrl = "https://" + req.headers.host + "/" + ++valorAcorte;
         arregloUrl[valorAcorte] = nuevaUrl;
         var urlJson = JSON.stringify({"url_original": urlDada, "url_corta":nuevaUrl});
         res.writeHead(200, {"content-type": "text/json"});
         res.end(urlJson);
+    } else{
+        var pathAcortado = req.url.slice(1);
+        if (arregloUrl[pathAcortado]){
+            console.log(arregloUrl[pathAcortado]);
+        }else{
+            console.log("error");
+        }
+        
     }
 }
