e384c73b6caead1930f7dcc443cbc932a8c243b9
Juan Cardona <juancruz.23@hotmail.com>
2016-02-25 19:55:28 +0000

    agregada redirección

--- api/acortador.js
+++ api/acortador.js
@@ -1,3 +1,4 @@
+var https = require("https");
 var arregloUrl = {};
 var valorAcorte = 0;
 module.exports = function(req,res){
@@ -5,17 +6,32 @@ module.exports = function(req,res){
     if (match && match[1] == "/new/"){
         var urlDada = match[2];
         var nuevaUrl = "https://" + req.headers.host + "/" + ++valorAcorte;
-        arregloUrl[valorAcorte] = nuevaUrl;
+        arregloUrl[valorAcorte] = urlDada;
         var urlJson = JSON.stringify({"url_original": urlDada, "url_corta":nuevaUrl});
         res.writeHead(200, {"content-type": "text/json"});
         res.end(urlJson);
     } else{
         var pathAcortado = req.url.slice(1);
-        if (arregloUrl[pathAcortado]){
-            console.log(arregloUrl[pathAcortado]);
+        var urlPedida = arregloUrl[pathAcortado];
+        if (urlPedida){
+            res.writeHead(301,{"Location":urlPedida});
+            res.end();
         }else{
-            console.log("error");
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.write('Error 404: resource not found.');
+            res.end();
         }
         
     }
+}
+
+var callbackSolicitud = function(response){
+    var datos = '';
+    response.on('data',function(data){
+        datos += data;
+    });
+    response.on('end',function(data){
+        datos += data;
+        console.log(datos);
+    });
 }
