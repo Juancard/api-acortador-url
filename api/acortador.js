8ea6739e50aad8f0c4b9201f7d59cedc32a4ae12
Juan Cardona <juancruz.23@hotmail.com>
2016-02-25 20:13:18 +0000

    agregado modulo shortid

--- api/acortador.js
+++ api/acortador.js
@@ -1,12 +1,15 @@
 var https = require("https");
+var shortid = require("shortid");
 var arregloUrl = {};
 var valorAcorte = 0;
 module.exports = function(req,res){
     var match = req.url.match(/(^\/new\/)(.+)/);
     if (match && match[1] == "/new/"){
         var urlDada = match[2];
-        var nuevaUrl = "https://" + req.headers.host + "/" + ++valorAcorte;
-        arregloUrl[valorAcorte] = urlDada;
+        var shortGenerado = shortid.generate();
+        var nuevaUrl = "https://" + req.headers.host + "/" + shortGenerado/*++valorAcorte*/;
+        arregloUrl[shortGenerado] = urlDada;
+        //arregloUrl[valorAcorte] = urlDada;
         var urlJson = JSON.stringify({"url_original": urlDada, "url_corta":nuevaUrl});
         res.writeHead(200, {"content-type": "text/json"});
         res.end(urlJson);
