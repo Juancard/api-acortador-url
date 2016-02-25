e61ed43496456d57008d2b14704ddbd8e13d570b
Juan Cardona <juancruz.23@hotmail.com>
2016-02-25 22:36:10 +0000

    errores json en url invalida y en url acortada inexistente

--- api/acortador.js
+++ api/acortador.js
@@ -1,4 +1,5 @@
 var shortid = require("shortid");
+var validUrl = require('valid-url');
 
 var urlGuardadas = {"N1-icw_sx": "http://blog.codinghorror.com/"}; //Cargo una url como ejemplo
 
@@ -6,6 +7,10 @@ module.exports = function(req,res){
     var match = req.url.match(/(^\/new\/)(.+)/);
     if (match && match[1] == "/new/"){
         var urlDada = match[2];
+        if (!validUrl.isUri(urlDada)){
+            return errorJson(res,"Url invalida");
+        }
+        console.log("entre");
         var shortUrl = buscarUrl(urlDada);
         if (!shortUrl) {
             shortUrl = shortid.generate();
@@ -22,11 +27,8 @@ module.exports = function(req,res){
             res.writeHead(301,{"Location":urlPedida});
             res.end();
         }else{
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.write('Error 404: resource not found.');
-            res.end();
+            errorJson(res,"No hay Url acortada para el input dado");
         }
-        
     }
 }
 
@@ -35,4 +37,10 @@ function buscarUrl(urlBuscada){
         if (urlGuardadas[key] == urlBuscada) {return key;}
     }
     return null;
+}
+
+function errorJson(res, mensaje){
+    res.writeHead(404, {'Content-Type': 'text/json'});
+    var err = JSON.stringify({"error":mensaje});
+    res.end(err);
 }
